3fd83c3eb312b2bea09dc966967404c4e0f61d49
GajutGinting13 <GajutGinting13@users.noreply.github.com>
2024-07-12 11:52:34 +0700

    update

--- database/connect.php
+++ database/connect.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "12345678";
-$dbname = "charger";
+$username = "u387258854_project";
+$password = "Project12345678";
+$dbname = "u387258854_charger";
 $koneksi = new mysqli($servername, $username, $password, $dbname);
 if ($koneksi->connect_error) {
     die("Koneksi gagal: " . $koneksi->connect_error);
